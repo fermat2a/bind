9d92822499f781ef31f4e2378af4415adbd3d7d5
Sascha Effert <fermat@douglas2a.de>
2023-02-25 15:26:44 +0100

    removed drecksserver from DNS Server List

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,5 @@ ssh root@dhcp1 systemctl restart bind9 && \
 echo "dhcp2:" && \
 scp * root@dhcp2:/etc/bind/ && \
 ssh root@dhcp2 systemctl restart bind9 && \
-echo "drecksserver:" && \
-scp * fermat@drecksserver:repositories/bind/ && \
-ssh fermat@drecksserver docker container restart bind && \
-echo "Successfully deployed and restarted DNS"
+echo "successfully deployed and restarted dns servers"
 
